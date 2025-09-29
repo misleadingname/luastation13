6cea70ed0774546c5f3c695fcacb857882daf6b3
misname <49401941+misleadingname@users.noreply.github.com>
2025-09-29 21:00:30 +0200

    Update init.lua

--- shared/init.lua
+++ shared/init.lua
@@ -19,8 +19,11 @@ function shared.load(args)
 	LS13.ECSManager = require("lib.concord")
 	LS13.Logging = require("shared.logging")
 	LS13.Util = require("shared.utilities")
-	LS13.Logging.PrintDebug(LS13.Util.Gilb())
 
+	require("shared.math")
+	require("shared.world")
+
+	LS13.Logging.PrintDebug(LS13.Util.Gilb())
 	DEBUG = LS13.Util.GetArgument("debug")
 
 	LS13.Logging.PrintLevel = DEBUG and 0 or 1
