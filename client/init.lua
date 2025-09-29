403c37942b59204a3891300e7493602f2e5648a4
ttmso <kleinvuur2011@gmail.com>
2025-09-29 14:36:38 +0200

    per sound volume and another lobby music

--- client/init.lua
+++ client/init.lua
@@ -1,44 +1,43 @@
 local states = {
-	Loading = require("client/states/loading"),
-	Debug = require("client/states/debug"),
-	Menu = require("client/states/menu"),
+    Loading = require("client/states/loading"),
+    Debug = require("client/states/debug"),
+    Menu = require("client/states/menu"),
 }
 
 local client = {}
 
 function client.load(args)
-	
-	LS13.States = states
-	LS13.UI = require("client/ui")
-	LS13.StateManager = require("lib/gamestatemanager/gameStateManager")
+    LS13.States = states
+    LS13.UI = require("client/ui")
+    LS13.StateManager = require("lib/gamestatemanager/gameStateManager")
 
-	LS13.StateManager:setState(states.Loading)
+    LS13.StateManager:setState(states.Loading)
 
-	if DEBUG then states.Debug:enter() end
+    if DEBUG then states.Debug:enter() end
 end
 
 function client.update(dt)
-	LS13.StateManager:update(dt)
-	-- client.UI.Update(dt)
+    LS13.StateManager:update(dt)
+    -- client.UI.Update(dt)
 
-	if DEBUG then states.Debug:update(dt) end
+    if DEBUG then states.Debug:update(dt) end
 end
 
 function love.draw()
-	LS13.StateManager:draw()
+    LS13.StateManager:draw()
 
-	love.graphics.setColor(1, 1, 1, 1)
-	-- LS13.UI.Draw()
+    love.graphics.setColor(1, 1, 1, 1)
+    -- LS13.UI.Draw()
 
-	if DEBUG then states.Debug:draw() end
+    if DEBUG then states.Debug:draw() end
 end
 
 function love.mousepressed(x, y, button)
-	LS13.UI.MousePressed(x, y, button)
+    LS13.UI.MousePressed(x, y, button)
 end
 
 function love.mousereleased(x, y, button)
-	LS13.UI.MouseReleased(x, y, button)
+    LS13.UI.MouseReleased(x, y, button)
 end
 
 return client
--- shared/prototype/parsers/sound.lua
+++ shared/prototype/parsers/sound.lua
@@ -1,19 +1,21 @@
 return function(node)
-	local data = {
-		id = node._attr and node._attr.Id and node._attr.Id,
-		type = "sound",
+    local data = {
+        id = node._attr and node._attr.Id and node._attr.Id,
+        type = "sound",
 
-		fileName = node.FileName and node.FileName,
-		soundMode = node.SoundMode and node.SoundMode,
-		looping = node.Looping and node.Looping == "true",
+        fileName = node.FileName and node.FileName,
+        soundMode = node.SoundMode and node.SoundMode,
+        looping = node.Looping and node.Looping == "true",
+        volume = node.Volume and node.Volume or 1.0,
 
-		name = node.Name and node.Name,
-		author = node.Author and node.Author
-	}
+        name = node.Name and node.Name,
+        author = node.Author and node.Author
+    }
 
-	local snd = love.audio.newSource(data.fileName, data.soundMode)
-	snd:setLooping(data.looping)
+    local snd = love.audio.newSource(data.fileName, data.soundMode)
+    snd:setLooping(data.looping)
+    snd:setVolume(data.volume)
 
-	data.sound = snd
-	LS13.AssetManager.Push(data, data.id)
+    data.sound = snd
+    LS13.AssetManager.Push(data, data.id)
 end
