75097bdc8ad1a2eca9f1712bde6a87179ba06d01
misname <49401941+misleadingname@users.noreply.github.com>
2025-10-01 00:48:52 +0200

    transform direction

--- shared/world/components/transform.lua
+++ shared/world/components/transform.lua
@@ -2,6 +2,7 @@ local ecs = LS13.ECSManager
 
 local transformComponent = ecs.component("Transform", function(c, position, z, rotation, scaleX, scaleY)
 	c.position = position or Vector2.new(0, 0)
+	c.direction = "left"
 	c.z = z or 0
 
 	c.rotation = rotation or 0
