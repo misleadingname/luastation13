b12b4d54fd546162b3f627136ee535c471d611b3
ttmso <kleinvuur2011@gmail.com>
2025-10-03 22:08:31 +0200

    wut da heck is a printer

--- shared/logging.lua
+++ shared/logging.lua
@@ -5,45 +5,45 @@ local Logging = {}
 
 local logs = {}
 
-local PRINTER_TYPE_DEBUG = 1
-local PRINTER_TYPE_INFO = 2
-local PRINTER_TYPE_WARN = 3
-local PRINTER_TYPE_ERROR = 4
-local PRINTER_TYPE_FATAL = 5
+local LOGGER_TYPE_DEBUG = 1
+local LOGGER_TYPE_INFO = 2
+local LOGGER_TYPE_WARN = 3
+local LOGGER_TYPE_ERROR = 4
+local LOGGER_TYPE_FATAL = 5
 
-local printerTypeStrLUT = {
-	[PRINTER_TYPE_DEBUG] = "[DEBUG]",
-	[PRINTER_TYPE_INFO]  = "[INFO ]",
-	[PRINTER_TYPE_WARN]  = "[WARN ]",
-	[PRINTER_TYPE_ERROR] = "[ERROR]",
-	[PRINTER_TYPE_FATAL] = "[FATAL]",
+local loggerTypeStrLUT = {
+	[LOGGER_TYPE_DEBUG] = "[DEBUG]",
+	[LOGGER_TYPE_INFO]  = "[INFO ]",
+	[LOGGER_TYPE_WARN]  = "[WARN ]",
+	[LOGGER_TYPE_ERROR] = "[ERROR]",
+	[LOGGER_TYPE_FATAL] = "[FATAL]",
 }
 
 -- for some fuckass reason using Color here causes init to hang
-local printerTypeColLUT = {
-	-- [PRINTER_TYPE_DEBUG] = Color.new(32, 32, 32),
-	-- [PRINTER_TYPE_INFO] = Color.new(135, 135, 230),
-	-- [PRINTER_TYPE_ERROR] = Color.new(220, 96, 96),
-	-- [PRINTER_TYPE_FATAL] = Color.new(255, 32, 32),
-	[PRINTER_TYPE_DEBUG] = { r = 90 / 255, g = 90 / 255, b = 90 / 255 },
-	[PRINTER_TYPE_INFO] = { r = 135 / 255, g = 135 / 255, b = 230 / 255 },
-	[PRINTER_TYPE_WARN] = { r = 255 / 255, g = 200 / 255, b = 0 / 255 },
-	[PRINTER_TYPE_ERROR] = { r = 220 / 255, g = 96 / 255, b = 96 / 255 },
-	[PRINTER_TYPE_FATAL] = { r = 255 / 255, g = 32 / 255, b = 32 / 255 },
+local loggerTypeColLUT = {
+	-- [LOGGER_TYPE_DEBUG] = Color.new(32, 32, 32),
+	-- [LOGGER_TYPE_INFO] = Color.new(135, 135, 230),
+	-- [LOGGER_TYPE_ERROR] = Color.new(220, 96, 96),
+	-- [LOGGER_TYPE_FATAL] = Color.new(255, 32, 32),
+	[LOGGER_TYPE_DEBUG] = { r = 90 / 255, g = 90 / 255, b = 90 / 255 },
+	[LOGGER_TYPE_INFO] = { r = 135 / 255, g = 135 / 255, b = 230 / 255 },
+	[LOGGER_TYPE_WARN] = { r = 255 / 255, g = 200 / 255, b = 0 / 255 },
+	[LOGGER_TYPE_ERROR] = { r = 220 / 255, g = 96 / 255, b = 96 / 255 },
+	[LOGGER_TYPE_FATAL] = { r = 255 / 255, g = 32 / 255, b = 32 / 255 },
 }
 
-local printerLevelTresholdLUT = {
-	[PRINTER_TYPE_DEBUG] = 0,
-	[PRINTER_TYPE_INFO] = 1,
-	[PRINTER_TYPE_WARN] = 2,
-	[PRINTER_TYPE_ERROR] = 3,
-	[PRINTER_TYPE_FATAL] = 4,
+local loggerLevelTresholdLUT = {
+	[LOGGER_TYPE_DEBUG] = 0,
+	[LOGGER_TYPE_INFO] = 1,
+	[LOGGER_TYPE_WARN] = 2,
+	[LOGGER_TYPE_ERROR] = 3,
+	[LOGGER_TYPE_FATAL] = 4,
 }
 
-local function makePrinter(printerType)
-	local typeStr = printerTypeStrLUT[printerType]
-	local typeCol = printerTypeColLUT[printerType]
-	local typeTreshold = printerLevelTresholdLUT[printerType]
+local function makeLogger(loggerType)
+	local typeStr = loggerTypeStrLUT[loggerType]
+	local typeCol = loggerTypeColLUT[loggerType]
+	local typeTreshold = loggerLevelTresholdLUT[loggerType]
 
 	return function(format, ...)
 		if Logging.PrintLevel > typeTreshold then
@@ -87,10 +87,10 @@ end
 Logging.Logs = logs
 Logging.PrintLevel = 0
 
-Logging.LogDebug = makePrinter(PRINTER_TYPE_DEBUG)
-Logging.LogInfo = makePrinter(PRINTER_TYPE_INFO)
-Logging.LogWarn = makePrinter(PRINTER_TYPE_WARN)
-Logging.LogError = makePrinter(PRINTER_TYPE_ERROR)
-Logging.LogFatal = makePrinter(PRINTER_TYPE_FATAL)
+Logging.LogDebug = makeLogger(LOGGER_TYPE_DEBUG)
+Logging.LogInfo = makeLogger(LOGGER_TYPE_INFO)
+Logging.LogWarn = makeLogger(LOGGER_TYPE_WARN)
+Logging.LogError = makeLogger(LOGGER_TYPE_ERROR)
+Logging.LogFatal = makeLogger(LOGGER_TYPE_FATAL)
 
 return Logging
