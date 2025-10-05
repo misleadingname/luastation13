03c272fea5a43479a6e6603fda1a56b8bb62825c
misname <49401941+misleadingname@users.noreply.github.com>
2025-10-05 18:08:53 +0200

    Update init.lua

--- client/ui/init.lua
+++ client/ui/init.lua
@@ -8,6 +8,7 @@ local systems = LS13.ECS.Systems
 local ui = {}
 local cursor = {
 	position = Vector2.new(0, 0),
+	activeButtons = {}
 }
 
 function ui.init()
@@ -32,54 +33,36 @@ end
 
 function ui.mousePressed(x, y, button)
 	cursor.position.x, cursor.position.y = x, y
+	cursor.activeButtons[button] = true
 	ui.world:emit("press", button)
 end
 
 function ui.mouseReleased(x, y, button)
 	cursor.position.x, cursor.position.y = x, y
+	cursor.activeButtons[button] = false
 	ui.world:emit("release", button)
 end
 
 function ui.test_scene()
-	local ent1 = LS13.ECSManager.entity("parent")
-	local ent2 = LS13.ECSManager.entity("child")
+	local container = LS13.ECSManager.entity("parent")
+	container:give("UiElement")
+	container:give("UiTransform", Vector2.new(32, 32), Vector2.new(200, 450))
+	container:give("UiLayout", "horizontal", Vector2.new(8, 8), 4, "begin", "center")
+	container:give("UiPanel")
 
-	ent1:give("UiElement", nil, { ent2 })
-	ent1:give("UiTarget")
-	ent1:give("UiTransform", Vector2.new(32, 32), Vector2.new(256, 128))
-	ent1:give("UiPanel")
-	ent1:give(
-		"UiLabel",
-		"AAAAAAAAAAAAAA\nAAAAAAAAAAAAAAAA\nAAAAAAAAAAAAAA",
-		Color.white,
-		"Font.Default",
-		"center",
-		"center"
-	)
+	local label1 = LS13.ECSManager.entity("child")
+	label1:give("UiElement", container)
+	label1:give("UiTransform", Vector2.new(0, 0), Vector2.new(100, 32))
+	label1:give("UiLabel", "Hello, World!")
 
-	ent1.UiTarget.onClick = function(button)
-		LS13.Logging.LogDebug("A pressed")
-	end
+	local label2 = LS13.ECSManager.entity("child")
+	label2:give("UiElement", container)
+	label2:give("UiTransform", Vector2.new(32, 0), Vector2.new(100, 23))
+	label2:give("UiLabel", "Hello, World!")
 
-	ent2:give("UiElement", ent1)
-	ent2:give("UiTarget")
-	ent2:give("UiTransform", Vector2.new(16, 64), Vector2.new(256, 128))
-	ent2:give("UiPanel")
-	ent2:give(
-		"UiLabel",
-		"BBBBBBBBBBBBBB\nBBBBBBBBBBBBBBBB\nBBBBBBBBBBBBBB",
-		Color.white,
-		"Font.Default",
-		"center",
-		"center"
-	)
-
-	ent2.UiTarget.onClick = function(button)
-		LS13.Logging.LogDebug("B pressed")
-	end
-
-	ui.world:addEntity(ent1)
-	ui.world:addEntity(ent2)
+	ui.world:addEntity(container)
+	ui.world:addEntity(label1)
+	ui.world:addEntity(label2)
 end
 
 return ui
